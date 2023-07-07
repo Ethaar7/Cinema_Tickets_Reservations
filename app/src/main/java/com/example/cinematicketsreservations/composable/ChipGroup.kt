d98dd6f133df74ddbdfbcc6dc7eb2c013b795859
Ethaar hussien <ethaar7735@gmail.com>
2023-07-07 17:04:12 +0300

    edit

--- app/src/main/java/com/example/cinematicketsreservations/composable/ChipGroup.kt
+++ app/src/main/java/com/example/cinematicketsreservations/composable/ChipGroup.kt
@@ -7,31 +7,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.graphics.Color.Companion.Transparent
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cinematicketsreservations.ui.theme.Primary
-import com.example.cinematicketsreservations.ui.theme.PrimaryTextColor
-import com.example.cinematicketsreservations.ui.theme.Secondary
 
 @Composable
 fun chipGroup() {
@@ -48,8 +40,9 @@ fun chipGroup() {
         modifier = Modifier
             .padding(top = 24.dp, start = 16.dp)
             .fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-    ) {
+        horizontalArrangement = Arrangement.Center,
+
+        ) {
 
 
         chipList.forEach { it ->
@@ -78,6 +71,7 @@ fun Chip(
             .wrapContentSize()
             .clip(CircleShape)
             .background(background)
+            .padding(horizontal = 4.dp)
             .border(
                 width = 1.dp,
                 color = if (isSelected) Transparent else Color.LightGray,
